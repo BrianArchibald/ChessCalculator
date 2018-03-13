115ce9f7552304e4fcaebc99db5dba1aa92d4392
Brian Archibald <BrianAllenArchibald@gmail.comgit config --listgit config --global user.email BrianAllenArchibald@gmail.com>
2018-03-12 19:57:41 -0500

    first commit
    chess calc

---
+++ ChessCalculator.js
@@ -0,0 +1,18 @@
+// Subjects current rating (R)
+
+// Get ratings of opponents
+
+// Calculate ratings difference between opponent and subject
+
+// What is the winning expectancies against each opponent 
+
+// Add up the winning expectancies (W)
+
+// What was subjects total score for the event (S)
+
+// Use a K factor of 10, although depends on federation (K)
+
+
+// Formula  Total = K * (S - W)
+
+// New rating is the old rating (R) + Total
