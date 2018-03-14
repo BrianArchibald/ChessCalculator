d1dd35b6686e473211bf8c4ed3b60c3cc4e4df29
Brian Archibald <BrianAllenArchibald@gmail.comgit config --listgit config --global user.email BrianAllenArchibald@gmail.com>
2018-03-13 20:32:10 -0500

    inputs added
    started adding opponents to array

--- ChessCalculator.js
+++ ChessCalculator.js
@@ -1,16 +1,56 @@
 // Subjects current rating (R)
-
+var originalRating = document.getElementById('originalRating').value;
+	console.log(originalRating);
 // Get ratings of opponents
 
-// Calculate ratings difference between opponent and subject
-	// Add up the total difference between opponent and subject
+// Put opponents in array
+
+
+// var opponents = document.getElementsByClassName('opponentRatings').value;
+	// ratings = [].map.call(opponents, function (rating) {
+	// console.log(rating.value);
+	// });
+
+
+	// [].forEach.call(opponents, function () {
+	//	console.log(opponents);
+	// });
+
+	// Array.prototype.forEach.call(opponents, function (opps) {
+	//	console.log(opps.tagName);
+	// });
+
+
+var ratings = [];
+
+var ratingsInput = document.getElementsByClassName("opponentRatings");
+
+function insert () {
+	ratings.push( ratingsInput.value );
+
+	clearAndShow();
+}
+
+function clearAndShow () {
+	ratingsInput.value = "";
+
+	messageBox.innerHTML = "";
+}
+
+
+
+
+// Calculate ratings difference between opponent and subject for each opponent
+// use forEach
+
 
 // What is the winning expectancies against each opponent 
 
 // Add up the winning expectancies (W)
-	// then compare to actual score of total games
+// use reduce
 
 // What was subjects total score for the event (S)
+// reduce
 
 // Use a K factor of 10, although depends on federation (K)
 
