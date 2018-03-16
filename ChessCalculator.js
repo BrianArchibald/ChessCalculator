278a5bdef2d466f096d6a2b17bc716e1dc2b7458
Brian Archibald <brianallenarchibald@gmail.com>
2018-03-15 19:15:35 -0500

    opp ratings, original rating done
    got input for opp rating and subject rating into arrays

--- ChessCalculator.js
+++ ChessCalculator.js
@@ -1,9 +1,24 @@
 // Subjects current rating (R)
-var originalRating = document.getElementById('originalRating').value;
-	console.log(originalRating);
-// Get ratings of opponents
+var originalRating = [];
+var originalRatingInput = document.getElementById('originalRating');
+var ratingDifference;
 
+// Get ratings of opponents
 // Put opponents in array
+var oppRatings = [];
+var ratingsInput = document.getElementsByClassName('opponentRating');
+
+function insert () {
+	for (var i = 0; i < ratingsInput.length; i++) {
+		if (typeof ratingsInput[i] !== "undefined") {
+			oppRatings.push(ratingsInput[i].value);
+		}
+	}
+	originalRating.push(originalRatingInput.value);
+}
+//-----------oppResults is not working or showing numbers in the array
+oppRatings = oppRatings.map(Number);
+
 
 
 // var opponents = document.getElementsByClassName('opponentRatings').value;
@@ -21,40 +36,59 @@ var originalRating = document.getElementById('originalRating').value;
 	// });
 
 
-var ratings = [];
 
-var ratingsInput = document.getElementsByClassName("opponentRatings");
+// var ratings = [];
 
-function insert () {
-	ratings.push( ratingsInput.value );
+// var ratingsInput = document.getElementsByClassName("opponentRatings");
 
-	clearAndShow();
-}
+// function insert () {
+// 	ratings.push( ratingsInput.value );
 
-function clearAndShow () {
-	ratingsInput.value = "";
+// 	clearAndShow();
+// }
 
-	messageBox.innerHTML = "";
-}
+// function clearAndShow () {
+// 	ratingsInput.value = "";
+
+// 	messageBox.innerHTML = "";
+// }
 
 
 
 
 // Calculate ratings difference between opponent and subject for each opponent
-// use forEach
+// use forEach     own rating - opp rating
+
+// var ratingDifference = [];
+// oppRatings.forEach (function(diff) {
+// 	var ratingDifference = originalRating - oppRatings
+
+// });
+
+//   or   
+
+oppRatings.map ( function (item) {
+	var ratingDifference = originalRating - item;
+});
 
 
-// What is the winning expectancies against each opponent 
 
-// Add up the winning expectancies (W)
+
+// What is the winning expectancies against each opponent ex. .64, .88 etc..
+
+// Add up the winning expectancies (W)  ex.  1.89
 // use reduce
 
-// What was subjects total score for the event (S)
-// reduce
+// What was subjects total score for the event (S) 2.5 pts
+// reducevar ratingDifference = oppRatings.map (function (value) {
+
 
 // Use a K factor of 10, although depends on federation (K)
 
 
-// Formula  Total = K * (S - W)
+// Formula -- Total = K * (S- W)   kfactor * (real score - expected score)
 
-// New rating is the old rating (R) + Total
+// New rating is the old rating (R) + Total (round down, no decimals)
+
+// Display new rating.
+// newRating.html = newRating
