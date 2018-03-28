cfb801d2782421ef787bd0902b3e87b697344321
BrianArchibald <brianallenarchibald@gmail.com>
2018-03-27 20:12:17 -0500

    added foreach's
    cleaned up code added foreachs

--- ChessCalculator.js
+++ ChessCalculator.js
@@ -4,8 +4,6 @@ var originalRating = 0;
 	var winExpects = [];
 	var stats = [];
 
-
-
 function insert () {
 
 	originalRating = 0;
@@ -14,8 +12,6 @@ function insert () {
 	winExpects = [];
 	stats = [];
 
-	
-	
 	var originalRatingInput = document.getElementById('originalRating');
 	var eventScoreInput = document.getElementById('eventScore');
 	var ratingsInput = document.getElementsByClassName('opponentRating');
@@ -31,89 +27,38 @@ function insert () {
 			
 			}
 		}
-		
-		// oppRatings = ratingsInput.map(function(ratingsIn){
-		// 	return parseInt(ratingsIn.value);
-		// })
-		
-		for (i = 0; i < oppRatings.length; i++) {
-	if (typeof oppRatings[i] !== 'undefined') {
-		stats.push((oppRatings[i] - originalRating) / 400);
-	}
+
+	// the foreach below wont give me a new rating value //////	
+
+	// ratingsInput.forEach(function (item) {
+	// oppRatings.push(item);
+	// });
+
+		oppRatings.forEach(function (item) {
+			stats.push((item-originalRating) / 400)
+		});
+
+		//P(A) = 1/(1+10m) where (power of m) is the rating difference (rating(B)-rating(A)) divided by 400.
+		// for (i = 0; i < stats.length; i++) {
+		// 	if (typeof stats[i] !== 'undefined') {
+		// winExpects.push(1 / (1 +(Math.pow(10, stats[i]))));
+		// 	}
+		// }
+
+		stats.forEach(function(item) {
+			winExpects.push(1 / (1 +(Math.pow(10, stats))))
+		});
+
+
+		var totalExpects = winExpects.reduce(function( accumulator, currentValue ) {return accumulator + currentValue;
+		}, 0);
+
+		// Use a K factor of 10, although depends on federation (K)
+		var kFactor = 10
+
+		var totalFormula = kFactor * (eventScore - totalExpects);
+
+		var newRating = Math.round(originalRating + totalFormula);
+
+		document.getElementById('newRating').value = newRating;
 }
-
-//P(A) = 1/(1+10m) where (power of m) is the rating difference (rating(B)-rating(A)) divided by 400.
-for (i = 0; i < stats.length; i++) {
-	if (typeof stats[i] !== 'undefined') {
-		winExpects.push(1 / (1 +(Math.pow(10, stats[i]))));
-	}
-}
-
-// Add up the winning expectancies (W)  ex.  1.89
-// use reduce
-
-var totalExpects = winExpects.reduce(function( accumulator, currentValue ) {return accumulator + currentValue;
-}, 0);
-
-// Use a K factor of 10, although depends on federation (K)
-var kFactor = 10
-
-// Formula -- total = K  (S- W)   kfactor  (event score - winning expectencies)
-// var total = kFactor  (eventScore - W);
-var totalFormula = kFactor * (eventScore - totalExpects);
-
-// New rating is the old rating (R) + Total (round down, no decimals)
-// newRating = originalRating + total
-var newRating = Math.round(originalRating + totalFormula);
-
-document.getElementById('newRating').value = newRating;
-}
-
-
-
-// Calculate ratings difference between opponent and subject for each opponent
-// use forEach     own rating - opp rating
-
-
-// oppRatings.forEach (function(diff) {
-// var ratingDifference = originalRating - oppRatings
-// console.log (ratingDifference);
-// });
-
-//   or   
-
-// // oppRatings.map ( function (item) {
-// // 	var ratingDifference = originalRating - item;
-// });
-// ---------------------------------------------------------Did not work----
-// var ratingDifference;
-// function difference (originalRating, oppRatings) {
-// 	for (i=0; i < oppRatings.length; i++) {
-// 		ratingDifference += originalRating - oppRatings[i];
-// 	}
-// 		return ratingDifference;
-// }
-// ------------------------------------------------------
-
-// const ratingDifference = oppRatings.map(x => originalRating - x);
-// console.log(ratingDifference);
-
-
-/*var ratingDifference = oppRatings.map(function(diff) {
-	return originalRating - diff;
-});*/
-
-// What is the winning expectancies against each opponent ex. .64, .88 etc..
-// var winExpect
-
-
-
-// New rating is the old rating (R) + Total (round down, no decimals)
-// newRating = originalRating + total
-
-
-
-// Display new rating.
-// newRating.html = newRating
-
-//testing new PC commit 
